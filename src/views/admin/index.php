f1079a00d81be7b3cd0d4b6ce8ff05ff04589fa6
vbognolo <106765986+vbognolo@users.noreply.github.com>
2023-12-13 01:55:05 +0100

    Update index.php

--- src/views/admin/index.php
+++ src/views/admin/index.php
@@ -1,18 +1,25 @@
 <?php
 declare(strict_types = 1);     
-//is_admin($session->role);                               
+//is_admin($session->role);      
+//session_start();
 
-if(!$id){                                              // If no valid id
-    include APP_ROOT . '/src/views/page-not-found.php';     // Page not found
+/*$admin = $_SESSION['admin'];
+
+if(!isset($admin)){
+   header('location:login.php');
+}    */                     
+
+if(!$id){                                                       // If no valid id
+    include APP_ROOT . '/src/views/page-not-found.php';         // Page not found
 }
 
-$user = $model->getUser()->get($id);                   // Get member data
-    if(!$user){                                          // If array is empty
+$user = $model->getUser()->get($id);                            // Get member data
+    if(!$user){                                                 // If array is empty
         include APP_ROOT . '/src/views/page-not-found.php';     // Page not found
     }
 
 // navigation menu data 
-$data['navigation'] = $model->getCategory()->getGroups(); 
+$data['navigation'] = $model->getCategory()->getAdminGroups(); 
 $data['categories'] = $model->getCategory()->getAll();  
 $data['group_count'] = $model->getCategory()->countG();  
 
