29f729867bd3dcc187500bc1265d89f7511ded73
vbognolo <valentina.bognolo@gmail.com>
2025-04-30 05:21:55 +0200

    not-used folders

--- src/views/not used/transport-json.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types = 1);   
-
-$id = intval($_GET['id_transport']);
-$transport = $model->getTransport()->get($id);
-
-header('Content-Type: application/json');
-echo json_encode($transport);
